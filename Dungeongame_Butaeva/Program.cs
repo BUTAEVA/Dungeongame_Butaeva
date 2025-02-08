007ac3279c3943c78ecc9f3b21b0445935906719
EllaПК <@DESKTOP-4QD7H14>
2025-02-08 15:17:59 +0300

    Комната с монстрами,ловушками и т.д, оружие меч или лук,сундуки с зельем

--- Dungeongame_Butaeva/Program.cs
+++ Dungeongame_Butaeva/Program.cs
@@ -10,6 +10,165 @@ namespace Dungeongame_Butaeva
     {
         static void Main(string[] args)
         {
+            Player player = new Player();
+            string[] dungeonMap = new string[10];
+            Random rand = new Random();   // Карта подземелья        
+            for (int i = 0; i < dungeonMap.Length - 1; i++) // Заполнение карты подземелья случайными событиями
+            {
+                int eventType = rand.Next(0, 5); // 0-4: монстр, ловушка, сундук, торговец, пустая комната
+                switch (eventType)
+                {
+                    case 0:
+                        dungeonMap[i] = "Monster";
+                        break;
+                    case 1:
+                        dungeonMap[i] = "Trap";
+                        break;
+                    case 2:
+                        dungeonMap[i] = "Chest";
+                        break;
+                    case 3:
+                        dungeonMap[i] = "Merchant";
+                        break;
+                    default:
+                        dungeonMap[i] = "Empty Room";
+                        break;
+                }
+            }
+            dungeonMap[9] = "Boss"; // Босс в последней комнате
+            for (int i = 0; i < dungeonMap.Length; i++)
+            {
+                Console.WriteLine($"Вы входите в комнату {i + 1}: {dungeonMap[i]}");
+
+                switch (dungeonMap[i])
+                {
+                    case "Monster":
+                        FightWithMonster(player);
+                        break;
+                    case "Trap":
+                        EncounterTrap(player);
+                        break;
+                    case "Chest":
+                        OpenChest(player);
+                        break;
+                    case "Merchant":
+                        VisitMerchant(player);
+                        break;
+                    case "Empty Room":
+                        Console.WriteLine("В этой комнате ничего нет.");
+                        break;
+                    case "Boss":
+                        FightWithBoss(player);
+                        break;
+                }
+                if (player.Health <= 0)
+                {
+                    Console.WriteLine("Вы погибли. Игра окончена.");
+                    return;
+                }
+            }
+            Console.WriteLine("Поздравляем! Вы победили в игре!"); // Игровой цикл
+        }
+        static void FightWithMonster(Player player)
+        {
+            Random rand = new Random();
+            int monsterHealth = rand.Next(20, 51); // Здоровье монстра от 20 до 50
+            Console.WriteLine($"Вы встретили монстра с {monsterHealth} HP!");
+
+            while (monsterHealth > 0 && player.Health > 0)
+            {
+                Console.WriteLine("Выберите оружие: 1 - Меч, 2 - Лук");
+                int choice = int.Parse(Console.ReadLine());
+
+                int damage = 0;
+                if (choice == 1) // Меч
+                {
+                    damage = rand.Next(10, 21);
+                }
+                else if (choice == 2 && player.Arrows > 0) // Лук
+                {
+                    damage = rand.Next(5, 16);
+                    player.Arrows--;
+                }
+                else if (choice == 2 && player.Arrows <= 0)
+                {
+                    Console.WriteLine("У вас нет стрел!");
+                    continue; // Пропускаем итерацию цикла
+                }
+
+                monsterHealth -= damage;
+                Console.WriteLine($"Вы нанесли {damage} урона монстру. У него осталось {monsterHealth} HP.");
+
+                if (monsterHealth > 0)
+                {
+                    int monsterDamage = rand.Next(5, 16);
+                    player.Health -= monsterDamage;
+                    Console.WriteLine($"Монстр атакует вас и наносит {monsterDamage} урона. У вас осталось {player.Health} HP.");
+                }
+            }
+
+            if (monsterHealth <= 0)
+            {
+                Console.WriteLine("Вы победили монстра!");
+            }
+        }
+
+        static void EncounterTrap(Player player)
+        {
+            Random rand = new Random();
+            int damage = rand.Next(10, 21);
+            player.Health -= damage;
+            Console.WriteLine($"Вы попали в ловушку и потеряли {damage} HP. У вас осталось {player.Health} HP.");
+        }
+
+        static void OpenChest(Player player)
+        {
+            Random rand = new Random();
+            int answer, correctAnswer;
+
+            // Генерация загадки
+            int a = rand.Next(1, 10);
+            int b = rand.Next(1, 10);
+            correctAnswer = a + b;
+            Console.WriteLine($"Для того,чтобы открыть сундук, решите: {a} + {b} = ?");
+            while (true)
+            {
+                answer = int.Parse(Console.ReadLine());
+                if (answer == correctAnswer)
+                {
+                    int lootType = rand.Next(0, 3); // 0 - зелье, 1 - золото, 2 - стрелы
+                    switch (lootType)
+                    {
+                        case 0:
+                            if (player.InventoryCount < 5)
+                            {
+                                player.Inventory[player.InventoryCount++] = "Potion";
+                                Console.WriteLine("УРААА,Вы нашли зелье!");
+                            }
+                            else
+                            {
+                                Console.WriteLine("Ваш инвентарь полон!");
+                            }
+                            break;
+                        case 1:
+                            player.Gold += 10;
+                            Console.WriteLine("ПОЗДРАВЛЯЮ,Вы нашли золото!");
+                            break;
+                        case 2:
+                            player.Arrows += 3;
+                            Console.WriteLine("ПРЕКРАСНО,Вы нашли стрелы!");
+                            break;
+                    }
+                    break; // Выход из цикла после успешного ответа
+                }
+                else
+                {
+                    Console.WriteLine("Ответ неверный. Попробуйте снова.") ;
+                }
+            }
         }
     }
 }
+        
+       
+

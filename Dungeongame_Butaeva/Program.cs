821a47577eb72e221df4ce4965fb2107eef680f0
EllaПК <@DESKTOP-4QD7H14>
2025-02-08 15:11:49 +0300

    Добавьте файлы проекта.

---
+++ Dungeongame_Butaeva/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dungeongame_Butaeva
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
